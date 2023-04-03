3ebb2c267448b4123a8a6b531f153369cc2fbcae
udhos <udhos>
2023-04-03 19:35:43 -0300

    Build role-aware image.

---
+++ build-docker-role.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+version=0.35.0r ;# r stands for role
+
+docker build -t udhos/csi-s3:$version -f Dockerfile.role . 
+
+echo "# test s3fs:"
+echo docker run --entrypoint /usr/bin/s3fs -ti --rm udhos/csi-s3:0.35.0r
