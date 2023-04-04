d6388dee894606a365e216198259dc35839293c0
udhos <udhos>
2023-04-04 20:11:58 -0300

    Fix tags.

--- build-docker-role.sh
+++ build-docker-role.sh
@@ -2,7 +2,9 @@
 
 version=0.35.0r ;# r stands for role
 
-docker build -t udhos/csi-s3:$version -f Dockerfile.role . 
+tag=udhos/csi-s3-driver:$version
+
+docker build -t $tag -f Dockerfile.role .
 
 echo "# test s3fs:"
-echo docker run --entrypoint /usr/bin/s3fs -ti --rm udhos/csi-s3-driver:0.35.0r
+echo docker run --entrypoint /usr/bin/s3fs -ti --rm $tag
